b709041119051390a6ecf86d020522bc0cad5513
Benedict <berjistech.gmail.com>
2024-03-09 23:51:27 +0300

    Add click event to course card component

--- src/app/components/course/course-card/course-card.component.ts
+++ src/app/components/course/course-card/course-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Course } from '../../../interfaces/course';
+import { Router } from '@angular/router';
+import { UUID } from 'crypto';
 
 @Component({
   selector: 'app-course-card',
@@ -10,7 +12,13 @@ export class CourseCardComponent {
   @Input() course: Course = {} as Course;
 
 
+  constructor(private router: Router) {}
+  
   generateBackgroundGradientFromImage(imageUrl: string) {
     return `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${imageUrl})`;
+  }
+
+  loadCourse(id: UUID){
+    this.router.navigate(['/courses/', id]);
   }
 }
