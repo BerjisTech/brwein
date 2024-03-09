eb75be8874a6ed599a04f6c4feae7cb436208d7a
Benedict <berjistech.gmail.com>
2024-03-09 21:20:52 +0300

    Add course module and routes

--- src/app/modules/course/course-routing.module.ts
+++ src/app/modules/course/course-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from '../../components/course/main/main.component';
+import { CoursesComponent } from '../../components/course/courses/courses.component';
+import { CourseComponent } from '../../components/course/course/course.component';
+import { LessonComponent } from '../../components/course/lesson/lesson.component';
+import { LessonsComponent } from '../../components/course/lessons/lessons.component';
+import { SettingsComponent } from '../../components/course/settings/settings.component';
+import { SearchComponent } from '../../components/course/search/search.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', component: CoursesComponent },
+      { path: 'settings', component: SettingsComponent },
+      { path: 'search', component: SearchComponent },
+      { path: ':course_id', component: CourseComponent },
+      { path: ':course_id/lessons', component: LessonsComponent },
+      { path: ':course_id/lesson/:lesson_id', component: LessonComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/course/course.module.ts
+++ src/app/modules/course/course.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MainComponent } from '../../components/course/main/main.component';
+import { CoursesComponent } from '../../components/course/courses/courses.component';
+import { CourseComponent } from '../../components/course/course/course.component';
+import { LessonComponent } from '../../components/course/lesson/lesson.component';
+import { LessonsComponent } from '../../components/course/lessons/lessons.component';
+import { SettingsComponent } from '../../components/course/settings/settings.component';
+import { SearchComponent } from '../../components/course/search/search.component';
 
 import { CourseRoutingModule } from './course-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    CoursesComponent,
+    CourseComponent,
+    LessonComponent,
+    LessonsComponent,
+    SettingsComponent,
+    SearchComponent
+  ],
   imports: [
+    RouterModule,
     CommonModule,
     CourseRoutingModule
   ]
